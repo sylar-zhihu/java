b302807d81d65c972632150964ce3286ecc99a05
sylar-zhihu <307508861@qq.com>
2018-08-02 18:56:56 +0800

    asd

---
+++ JAVA_WEB/Hibernate/hibernate_1500_many2many_1/src/com/bjsxt/hibernate/Teacher.java
@@ -0,0 +1,53 @@
+package com.bjsxt.hibernate;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+public class Teacher {
+    private int id;
+
+    private String name;
+
+    private Set<Student> students = new HashSet<Student>();
+
+    @Id
+    @GeneratedValue
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @ManyToMany
+    // name 生成中间表的名字
+    //  joinColumns 当前表对应的字段
+    //  inverseJoinColumns 应严格表对应的字段
+    @JoinTable(name = "t_s", joinColumns = { @JoinColumn(name = "teacher_id") }, inverseJoinColumns = { @JoinColumn(name = "student_id") })
+    public Set<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(Set<Student> students) {
+        this.students = students;
+    }
+}
