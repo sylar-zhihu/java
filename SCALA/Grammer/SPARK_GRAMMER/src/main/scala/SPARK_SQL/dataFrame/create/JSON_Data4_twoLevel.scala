89423f2a0ab16b1d117b1f6a4a482ad8ca33f15b
sylar-zhihu <307508861@qq.com>
2019-02-17 10:29:38 +0800

    两种json

--- SCALA/Grammer/SPARK_GRAMMER/src/main/scala/SPARK_SQL/dataFrame/create/JSON_Data4_twoLevel.scala
+++ SCALA/Grammer/SPARK_GRAMMER/src/main/scala/SPARK_SQL/dataFrame/create/JSON_Data4_twoLevel.scala
@@ -1,7 +1,6 @@
 package SPARK_SQL.dataFrame.create
 
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.types._
 
 /**
   * 用途：创建sparkSession
@@ -9,7 +8,7 @@ import org.apache.spark.sql.types._
   * 日期:2019/1/29 12:31
   * 所有的读取类型都看作是String
   */
-object JSON_Data3_allString {
+object JSON_Data4_twoLevel {
 
   def main(args: Array[String]): Unit = {
     //创建sparkSession()并设置App名称
@@ -19,12 +18,14 @@ object JSON_Data3_allString {
       .getOrCreate()
 
 
-    val df = sparkSession.read.option("primitivesAsString", "true").json("C://employees.json")
+    val df = sparkSession.read.option("primitivesAsString", "true").json("C:\\data\\employees2.json")
 
     // 显示
     df.show()
     // 会自动推断类型
     df.printSchema()
+    // 得到某一个列
+    df.select("name.name1").show()
 
     // 关闭
     sparkSession.stop()
